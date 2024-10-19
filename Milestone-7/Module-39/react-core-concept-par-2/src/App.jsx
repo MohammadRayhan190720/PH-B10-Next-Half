c36bdaf4d361ea19d6ebf6b66fd31cb6b156a4b7
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-10-19 10:11:04 +0600

    react core concept part-2

--- Milestone-7/Module-39/react-core-concept-par-2/src/App.jsx
+++ Milestone-7/Module-39/react-core-concept-par-2/src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Counter from './Counter';
 import Team from './Team';
+import Users from './Users';
 
 function App() {
   
@@ -21,6 +22,7 @@ function App() {
     <>
       
       <h3> React Core Concepts Part-2</h3>
+      <Users></Users>
 
       <Team></Team>
       

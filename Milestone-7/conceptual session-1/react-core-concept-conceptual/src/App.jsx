0d24bc0b6878d683d8557f321db4698b348c154c
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-10-19 19:54:37 +0600

    learn react

--- Milestone-7/conceptual session-1/react-core-concept-conceptual/src/App.jsx
+++ Milestone-7/conceptual session-1/react-core-concept-conceptual/src/App.jsx
@@ -1,7 +1,17 @@
 import Products from './components/Products'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+
+  const[count,setCount] = useState(0)
+
+  const addCount = () =>{
+    setCount(count + 1);
+  }
+  const removeCount = () =>{
+    setCount(count - 1);
+  }
   
   const allProducts = {
     name : "serect",
@@ -13,6 +23,11 @@ function App() {
       
       <h3>React-core-concept-conceptual</h3>
       <Products info={allProducts}></Products>
+
+      <h3>Count :{count} </h3>
+
+      <button onClick={addCount}>Add Count</button>
+      <button onClick={removeCount}> Remove Count</button>
      
      
     </>

c10172d5698861d690256deb623e77fad5cd51c4
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-10-26 14:55:05 +0600

    another practice project

---
+++ Milestone-7/conceptual-seasion-2/react-practice-project-2/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h2 className='text-5xl font-bold bg-gray-500'>My Country</h2>
+     
+      
+    </>
+  )
+}
+
+export default App

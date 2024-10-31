6b909e1b7815c775fecbb7521c4eb053d349d26b
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-10-31 22:37:01 +0600

    react router concept

--- Milestone-8/Module-45/react-router-concept/src/main.jsx
+++ Milestone-8/Module-45/react-router-concept/src/main.jsx
@@ -3,19 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from './component/Home/Home.jsx';
 
 const router = createBrowserRouter([{
     path : "/",
-    element : <div>Hellow Form React Router</div>
+    element : <Home></Home>
 },
-{
-  path : "/about",
-  element : <div>This is About Page</div>
-},
-{
-  path : "/contact",
-  element : <div>Call Me Right Now!</div>
-}
+// {
+//   path : "/about",
+//   element : <div>This is About Page</div>
+// },
+// {
+//   path : "/contact",
+//   element : <div>Call Me Right Now!</div>
+// }
 ])
 
 createRoot(document.getElementById('root')).render(

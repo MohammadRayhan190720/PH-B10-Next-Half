169050a09cf3e46103f0fdbe107ea2ec222481bb
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-11-02 19:51:32 +0600

    Project basic setup done

---
+++ Milestone-8/Module-47/boi-poka-book-vibe/src/components/DashBoard/DashBoard.jsx
@@ -0,0 +1,11 @@
+
+
+const DashBoard = () => {
+  return (
+    <div>
+      <h2>Welcome to DashBoard!</h2>
+    </div>
+  );
+};
+
+export default DashBoard;
---
+++ Milestone-8/Module-47/boi-poka-book-vibe/src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+
+const ErrorPage = () => {
+  return (
+    <div className="flex justify-center items-center border border-red-700 min-h-screen">
+      <h3 className="text-5xl font-bold">Page not Found!!!</h3>
+      
+    </div>
+  );
+};
+
+export default ErrorPage;

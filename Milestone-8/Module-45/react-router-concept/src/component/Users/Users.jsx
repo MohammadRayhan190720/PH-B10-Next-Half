6636a134c4bd853c302dc2a1cfb0c6caa907ca2a
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-11-02 10:57:06 +0600

    router adding in a components

--- Milestone-8/Module-45/react-router-concept/src/component/Users/Users.jsx
+++ Milestone-8/Module-45/react-router-concept/src/component/Users/Users.jsx
@@ -1,9 +1,13 @@
+import { useLoaderData } from "react-router-dom";
 
 
 const Users = () => {
+  const users = useLoaderData()
+
+
   return (
     <div>
-      <h3>This is User Components</h3>
+      <h3>Our Users : {users.length}</h3>
       <p>Our Users is Very Good and Love us!</p>
       
     </div>

f3c620e96fb7d87e2b82a32e7157de525a68d736
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-11-02 10:25:04 +0600

    router adding in a components

--- Milestone-8/Module-45/react-router-concept/src/component/Header/Header.jsx
+++ Milestone-8/Module-45/react-router-concept/src/component/Header/Header.jsx
@@ -1,12 +1,13 @@
-
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div>
       <h3>Navbar</h3>
       <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact Us</a>
+        <Link to ="/">Home</Link>
+        <Link to ="/about">About</Link>
+        <Link to ="/contact">Contact Us</Link>
+        
       </nav>
       
     </div>

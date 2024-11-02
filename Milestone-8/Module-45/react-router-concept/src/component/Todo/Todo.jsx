e4e797843f58ae97a22658a384862e652e33ec97
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-11-02 15:16:01 +0600

    react router

--- Milestone-8/Module-45/react-router-concept/src/component/Todo/Todo.jsx
+++ Milestone-8/Module-45/react-router-concept/src/component/Todo/Todo.jsx
@@ -1,8 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Todo.css'
 const Todo = ({todo}) => {
   console.log(todo);
+
+    const navigate = useNavigate();
+
   const{userId,id,title,completed} = todo;
+  
+  const handleDetails = () => {
+    navigate(`/todo/${id}`)
+  }
+
+
+
+
   return (
     <div className="todoStyle">
       <h2>Todos ID :{id} </h2>
@@ -10,7 +21,7 @@ const Todo = ({todo}) => {
       <p>IsCompleted : {completed}</p>
       <Link to={`/todo/${id}`}>View Details</Link>
 
-      
+      <button onClick={handleDetails}>Click hare for Show Details</button>
     </div>
   );
 };

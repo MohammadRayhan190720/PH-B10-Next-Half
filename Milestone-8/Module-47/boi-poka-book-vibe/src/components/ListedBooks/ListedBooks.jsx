64ca675507884547cdbc829018a8fcbda63b4a96
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-11-04 10:25:07 +0600

    react tab

--- Milestone-8/Module-47/boi-poka-book-vibe/src/components/ListedBooks/ListedBooks.jsx
+++ Milestone-8/Module-47/boi-poka-book-vibe/src/components/ListedBooks/ListedBooks.jsx
@@ -2,21 +2,21 @@ import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
-import { getstoredReadList } from "../../Utilities/addToDb";
+import { getstoredReadList, getstoredWishList } from "../../Utilities/addToDb";
 import Book from "../Book/Book";
 
 const ListedBooks = () => {
 
   const [readList,setReadList] = useState([])
 
+  const [wishList,setWishList] = useState([])
+
   const allBooksData = useLoaderData();
 
+  //add to read list
   useEffect(() =>{
     const storedReadList = getstoredReadList();
     const storedReadListInt = storedReadList.map(id => parseInt(id))
-
-    
-
 
     const readBooksList = allBooksData.filter(book => storedReadListInt.includes(book.bookId))
   
@@ -24,10 +24,19 @@ const ListedBooks = () => {
   
   },[])
 
+  //addTo Wish List
+  useEffect(()=>{
+    const storedWishList = getstoredWishList();
+    const storedWishListInt = storedWishList.map(id => parseInt(id))
+
+    const wishListBooks = allBooksData.filter(book => storedWishListInt.includes(book.bookId))
+
+    setWishList(wishListBooks);
+  },[])
+
 
   return (
-    <div>
-     
+    <div className="mt-5">
       <Tabs>
         <TabList>
           <Tab>Read List</Tab>
@@ -35,15 +44,24 @@ const ListedBooks = () => {
         </TabList>
 
         <TabPanel>
-          <h2>Read List = {readList.length}</h2>
+          <h2 className="text-3xl font-bold text-center my-6">
+            Total Read Books {readList.length}
+          </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-            {
-              readList.map(book => <Book key={book.bookId} book={book}></Book>)
-            }
+            {readList.map((book) => (
+              <Book key={book.bookId} book={book}></Book>
+            ))}
           </div>
         </TabPanel>
         <TabPanel>
-          <h2>Any content 2</h2>
+          <h2 className="text-3xl font-bold text-center my-6">
+            Total WishList Books {wishList.length}
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            {wishList.map((book) => (
+              <Book key={book.bookId} book={book}></Book>
+            ))}
+          </div>
         </TabPanel>
       </Tabs>
     </div>

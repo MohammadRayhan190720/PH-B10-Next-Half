4835597d277db2e41f76e57488d70e6c761122bf
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-11-04 19:45:21 +0600

    coffee-book

---
+++ Milestone-8/conceptual-1/coffee-book-vibe/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+function App() {
+
+  return (
+    <>
+     
+      <h1 className="text-5xl text-center">Vite + React</h1>
+      <button className="btn btn-primary">Click Me</button>
+     
+    </>
+  )
+}
+
+export default App

42db720cdfd542e9b14bfcd8e70ef7775d161472
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-11-09 10:47:08 +0600

    recap-react

--- Milestone-8/conceptual-recap-1/react-recap-1/src/App.jsx
+++ Milestone-8/conceptual-recap-1/react-recap-1/src/App.jsx
@@ -1,13 +1,23 @@
 
+import { useState } from 'react'
 import './App.css'
+import Button from './components/Button'
 
 function App() {
+
+  const [count,setCount] = useState(5)
+
+  const countHandler = () =>{
+    setCount(count + 1)
+  }
  
 
   return (
     <>
      
       <h1>React Recap-1</h1>
+      <Button onClick={countHandler} count={count}></Button>
+      <Button onClick={countHandler} count={count}></Button>
 
     </>
   )

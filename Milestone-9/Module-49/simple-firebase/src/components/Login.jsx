7447bc61636a68e8122a82a848b4c83ef458211b
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-11-10 21:13:12 +0600

    firebase

--- Milestone-9/Module-49/simple-firebase/src/components/Login.jsx
+++ Milestone-9/Module-49/simple-firebase/src/components/Login.jsx
@@ -1,7 +1,10 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import auth from "../FIrebase/Firebase.init";
+import { useState } from "react";
 
 const Login = () => {
+
+  const [user,setUser] = useState(null)
 
   const provider = new GoogleAuthProvider;
 
@@ -10,15 +13,46 @@ const Login = () => {
     signInWithPopup(auth, provider)
     .then((result)=>{
       console.log(result);
+      setUser(result.user);
     })
     .catch((error) => {
       console.log(error);
+      setUser(null);
     })
   }
+
+  const handleGoogleSignOut = ()=>{
+    signOut(auth).then(()=>{
+      setUser(null);
+    })
+    .catch((error) => {
+      setUser(null);
+    })
+  }
+
+
+
   return (
     <div>
-      <button onClick={handleGoogleSignIn} className='px-5 py-3 border'>Login with Google</button>
       
+      {user ? (
+        <button onClick={handleGoogleSignOut} className="px-5 py-3 border">
+          Sing Out
+        </button>
+      ) : (
+        <button onClick={handleGoogleSignIn} className="px-5 py-3 border">
+          Login with Google
+        </button>
+
+      )}
+
+      {user && (
+        <div>
+          <h3>{user.displayName}</h3>
+          <p>{user.email}</p>
+          <img src={user.photoURL} alt="" />
+        </div>
+      )}
     </div>
   );
 };

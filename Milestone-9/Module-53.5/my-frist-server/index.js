3cef266eb49782d74bb9956c22b3d13673e8bdd0
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-11-24 09:45:03 +0600

    nodemon install

---
+++ Milestone-9/Module-53.5/my-frist-server/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req,res)=>{
+  res.send("Welcome my frist server!")
+})
+
+app.get('/data',(req,res)=>{
+  res.send("More data comeing sooooooooooooooon!")
+})
+
+app.listen(port, ()=>{
+  console.log(`My Frist server running on port ${port}`)
+})
---
+++ Milestone-9/Module-53.5/my-phone-server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const phones = require('./phones.json');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res)=>{
+  res.send('My Phone Server is runnnnnnnning now!')
+})
+
+app.get('/contact',(req, res)=>{
+  res.send('My contact server is running noooooooow!')
+})
+
+app.get('/phones', (req,res)=>{
+  res.send(phones)
+})
+
+app.get('/phones/:id', (req, res)=>{
+  const id = parseInt(req.params.id);
+  console.log("i need data for Id", id)
+
+  const phone = phones.find(phone => phone.id === id) ||{};
+  res.send(phone);
+})
+
+app.listen(port, ()=>{
+  console.log(`My Phone server is running at Port ${port}`)
+})

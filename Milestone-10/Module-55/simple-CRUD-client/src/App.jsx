6a15e3b90c608249ceca4676ba56ff2a923a7cc7
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-11-29 19:46:23 +0600

    learning CRUD operation

--- Milestone-10/Module-55/simple-CRUD-client/src/App.jsx
+++ Milestone-10/Module-55/simple-CRUD-client/src/App.jsx
@@ -3,10 +3,42 @@ import './App.css'
 
 function App() {
 
+  const handleSubmit = e =>{
+    e.preventDefault();
+
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = {name,email}
+    console.log(user);
+
+    fetch("http://localhost:5000/users",{
+      method: 'POST',
+      headers:{
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data =>{
+      console.log( 'new user' , data);
+    })
+
+  }
+
   return (
     <>
 
       <h1>Simple CRUD Client</h1>
+      <form onSubmit={handleSubmit}>
+        <h3>User Information</h3>
+        <input type="text" name="name" id="" placeholder='name' />
+        <br></br>
+        <input type="email" name="email" id="" placeholder='email' />
+        <br></br>
+
+        <button type="submit">Submit</button>
+      </form>
 
 
     </>

d25afef0213e5f1ecf9611ff0c29c85a06974741
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-11-30 09:13:03 +0600

    learning CRUD operation

--- Milestone-10/Module-55/simple-CRUD-client/src/components/Users.jsx
+++ Milestone-10/Module-55/simple-CRUD-client/src/components/Users.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 const Users = () => {
   const loadedUsers = useLoaderData()
@@ -26,6 +26,10 @@ const Users = () => {
       <h3>total users: {users.length}</h3>
       {
         users.map(user => <p key={user._id}>{user.name}:{user.email}
+        <Link to={`/update/${user._id}`}>
+        <button>Update</button>
+        </Link>
+
         <button onClick={()=>{handleDelete(user._id)}}>Delete</button>
         </p>)
       }
--- Milestone-10/Module-55/simple-CRUD-client/src/main.jsx
+++ Milestone-10/Module-55/simple-CRUD-client/src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Users from './components/Users.jsx';
+import Update from './components/Update.jsx';
 
 const router = createBrowserRouter([
   {
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
     element: <Users></Users>,
     loader: () => fetch("http://localhost:5000/users")
   },
+  {
+    path:"/update",
+    element: <Update></Update>,
+  }
 ]);
 
 createRoot(document.getElementById("root")).render(

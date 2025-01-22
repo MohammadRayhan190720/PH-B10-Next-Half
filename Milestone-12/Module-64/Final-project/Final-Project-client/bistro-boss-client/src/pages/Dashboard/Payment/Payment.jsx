b858cce637be4e7e8082329f6a80189fe8845b6d
MohammadRayhan190720 <rafirayhan260@gmail.com>
2025-01-22 23:15:44 +0600

    added stripe

--- Milestone-12/Module-64/Final-project/Final-Project-client/bistro-boss-client/src/pages/Dashboard/Payment/Payment.jsx
+++ Milestone-12/Module-64/Final-project/Final-Project-client/bistro-boss-client/src/pages/Dashboard/Payment/Payment.jsx
@@ -1,20 +1,25 @@
 
 import DynamicTitle from "../../../components/shared/DynamicTitle";
 import { loadStripe } from "@stripe/stripe-js";
+import { Elements} from '@stripe/react-stripe-js'
+import CheckOutForm from '../Payment/CheckOutForm'
 
 
 const Payment = () => {
 
 
 //todo: add publishable key
-const stripePromise = loadStripe('')
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 
   return (
     <div>
       <DynamicTitle Heading={"Payment"} subHeading={"Please Pay Frist"}>
       </DynamicTitle>
       <div>
-        <Elements
+       <Elements stripe={stripePromise}>
+        <CheckOutForm/>
+       </Elements>
+
       </div>
 
 
